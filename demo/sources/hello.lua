b19b0aee7d78d81d6c18216f07c3681e9ddda463
dytu <notdyl2010@gmail.com>
2026-06-29 14:02:17 -0400

    chipped at the manual a lil bit more

--- demo/sources/hello.lua
+++ demo/sources/hello.lua
@@ -2,14 +2,12 @@ hello = {} -- make a table for the demo, this will reoccur in all the other demo
 
 function hello.tick()
 	clear(0) -- clear the framebuffer
-
+		
 	-- this renders the default palette
-	for index = 0, 15 do -- a for loop that handles the palette index
+	for index = 0, 16 do -- a for loop that handles the palette index
 		-- for each index, render the pixel at y = 38 and x = 32 but offset it by its own index
 		pixel(32 + index, 38, index)
 	end
-	
-	poke2((0x00000 + (24 * 128 + 13) * 2), 0xFFFF)
 	-- renders the text on the screen. "\n" prints it to the second line
 	text("hello!\nwelcome to yf", 32, 43)
 end
